4300b2ae9508dcb936d89c708bd9090a7c01aa4a
Alcin Snowlina <alcinsnowlinars@gmail.com>
2025-04-18 12:34:50 +0530

    feat:hotel_management

--- hospitality/guest_house/doctype/member_details/member_details.py
+++ hospitality/guest_house/doctype/member_details/member_details.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2025, Admin and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class MemberDetails(Document):
-	pass
+
+	@frappe.whitelist()
+	def get_renewal(self):
+		doc=frappe.get_doc("Membership", self.membership_id)
+		for item in self.renewal_table:
+			item.membership_type = doc.membership_type,
+			item.start_date = doc.start_date,
+			item.end_date = doc.end_date,
+
+
