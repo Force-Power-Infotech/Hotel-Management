ae77992086c649b5d641eded779944a8c0e9d296
chethank1407 <chethank1407@gmail.com>
2025-04-22 15:01:15 +0530

    feat: sales invoice creation

--- hospitality/guest_house/doctype/room_reservation/room_reservation.py
+++ hospitality/guest_house/doctype/room_reservation/room_reservation.py
@@ -7,7 +7,74 @@ import datetime
 from frappe.desk.calendar import get_event_conditions
 
 class RoomReservation(Document):
-	pass
+
+	def on_submit(self):
+		customer = ""
+
+		if self.member_id:
+			member = frappe.get_doc("Member Details", self.member_id)
+			customer = member.customer
+
+		# elif self.guest_id:
+		# 	guest = frappe.get_doc("Guest Details", self.guest_id)
+		# 	customer = guest.customer
+
+		if not customer:
+			frappe.throw("No customer linked to this reservation.")
+
+		si = frappe.new_doc("Sales Invoice")
+		si.customer = customer
+		si.due_date = frappe.utils.today()
+		si.selling_price_list = "Regular Member"
+
+		if self.sales_item and self.checkin_date and self.checkout_date:
+			days = (frappe.utils.getdate(self.checkout_date) - frappe.utils.getdate(self.checkin_date)).days
+			if days > 0:
+				rate = get_valid_item_price(self.sales_item, "Standard Selling")
+				si.append("items", {
+					"item_code": self.sales_item,
+					"qty": days,
+					"rate": rate
+				})
+
+		for item in self.additional_purchases:
+			if item.item_code and item.quantity:
+				rate = get_valid_item_price(item.item_code, "Standard Selling")
+				si.append("items", {
+					"item_code": item.item_code,
+					"qty": item.quantity,
+					"rate": rate
+				})
+
+		if not si.items:
+			frappe.throw("No items to bill.")
+
+		si.insert()
+		si.submit()
+
+		frappe.msgprint(f"Sales Invoice <a href='/app/sales-invoice/{si.name}'>{si.name}</a> created for {customer}")
+
+
+def get_valid_item_price(item_code, price_list):
+	today_date = frappe.utils.today()
+
+	item_price = frappe.db.sql("""
+		SELECT price_list_rate
+		FROM `tabItem Price`
+		WHERE item_code = %s
+		AND price_list = %s
+		AND (valid_upto IS NULL OR valid_upto >= %s)
+		ORDER BY 
+			CASE WHEN valid_upto IS NULL THEN 1 ELSE 0 END,
+			valid_upto ASC
+		LIMIT 1
+	""", (item_code, price_list, today_date), as_dict=1)
+	frappe.log_error(title="Test", message=item_price)
+
+	if item_price:
+		return item_price[0].price_list_rate
+
+	return 0
 
 @frappe.whitelist()
 def get_events(start, end, filters=None):
